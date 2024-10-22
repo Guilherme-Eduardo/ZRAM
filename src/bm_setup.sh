b6a5e00ca769bf9df797380de26f714ec03d360a
Gustavo Frehse <frehsegustavo@gmail.com>
2024-10-22 14:38:52 -0300

    feat: anotacoes do zanata sobre o artigo

--- src/bm_setup.sh
+++ src/bm_setup.sh
@@ -4,7 +4,7 @@ source ./bm_functions.sh # Funcoes
 
 ### Variaveis de ambiente ###
 # BENCHMARKS=('is' 'ep' 'cg' 'mg' 'ft' 'bt' 'sp' 'lu') # Definir os benchmarks que serão executados
-# BENCH_CLASSES=('A' 'B' 'C' 'D') # Classes a serem testadas
+# BENCH_CLASSES=('A' 'B' 'C') # Classes a serem testadas
 # ZRAM_PORC=(0 25 50 75 100) # Porcentagem de ZRAM a serem testadas
 LOG_ARCHIVE="BM_ZRAM_$(date +"%Y-%m-%d_%H:%M:%S").log" # Criar arquivo de log da execucao
 RESULT_SOURCE="../results" # Onde sera guardado os resultados (*.txt)
