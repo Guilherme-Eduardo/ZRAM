b14277166c395a48a36575983c5f75268b8e4aaa
Gustavo Frehse <frehsegustavo@gmail.com>
2024-10-22 14:33:05 -0300

    feat: assinatura da funcao de habilitar zram

--- src/bm_functions.sh
+++ src/bm_functions.sh
@@ -12,18 +12,47 @@ function write_logs() {
 function show_zram_stats() {
     AN=$1 # Nome do arquivo
 
-    # Preparacao dos reusltados
+    # Preparacao dos resultados
 
     # Impressao dos resultados
-    echo >> $AN
     echo "---------------- RESULTADOS ZRAM (bytes) ---------------- " >> $AN
-    printf "%30s | %20d\n" "Swaps realizados" 0 >> $AN
-    printf "%30s | %20d\n" "Dados originais" 0 >> $AN
-    printf "%30s | %20d\n" "Dados comprimidos" 0 >> $AN
-    printf "%30s | %20d\n" "Memoria usada total" 0 >> $AN
-    printf "%30s | %20d\n" "Taxa de compressao" 0 >> $AN
+    printf "%30s | %20d\n" "Swaps realizados" "$(cat cat_file.txt)" >> $AN
+    printf "%30s | %20d\n" "Dados originais" "$(cat cat_file.txt)" >> $AN
+    printf "%30s | %20d\n" "Dados comprimidos" "$(cat cat_file.txt)" >> $AN
+    printf "%30s | %20d\n" "Memoria usada total" "$(cat cat_file.txt)" >> $AN
+    printf "%30s | %20d\n" "Taxa de compressao" "$(cat cat_file.txt)" >> $AN
     echo "--------------------------------------------------------- " >> $AN
-    echo >> $AN
+}
+
+### DESABILITAR ZRAM ###
+function disable_zram() {
+    echo "disabilitando zram"
+}
+
+### HABILITAR ZRAM ###
+function enable_zram() {
+    PORC=$1 # Porcentagem de zram
+    LOG_ARCHIVE=$2
+
+    # Caso de entradas de ZRAM impossiveis
+    if [ $PORC -gt 100 ]; then
+        write_logs $LOG_ARCHIVE "[ERRO] Porcentagem de ZRAM mal definida: ${PORC}"
+        return 1 
+    fi
+    if [ $PORC -lt 0 ]; then
+        write_logs $LOG_ARCHIVE "[ERRO] Porcentagem de ZRAM mal definida: ${PORC}"
+        return 1 
+    fi
+
+    # Desabilitar zram
+    if [ $PORC -eq 0 ]; then
+        disable_zram
+    else
+        # Habilitar zram
+        echo "habilitando zram..."
+    fi
+
+    return 0
 }
 
 ### FAZER TESTE ###
