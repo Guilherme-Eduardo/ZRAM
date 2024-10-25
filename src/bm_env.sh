14ce088fdf14c4b25044271c20d87d761ebf0a07
Gustavo Frehse <frehsegustavo@gmail.com>
2024-10-25 12:59:47 +0000

    update: nome do log e debug mode

--- src/bm_env.sh
+++ src/bm_env.sh
@@ -7,11 +7,11 @@ BENCH_CLASSES=('S') # Classes a serem testadas (S (fast to test), A, B, C (mediu
 ZRAM_PORC=(0 50) # Porcentagem de ZRAM a serem testadas
 COMPRESSION_ALGS=('null')
 
-LOG_ARCHIVE="BM_ZRAM_$(date +"%Y-%m-%d_%H:%M:%S").log" # Criar arquivo de log da execucao
+LOG_ARCHIVE="BM_ZRAM_$(date +"%d%m%Y_%H:%M:%S").log" # Criar arquivo de log da execucao
 
 RESULT_SOURCE="../results" # Onde sera guardado os resultados (*.log)
 BENCHMARK_DIR="./NPB-OMP" # Onde esta localizado os benchmarks
 
 ### Variaveis de debug ###
 
-NO_ZRAM_DEBUGGER=1 # Ative se quiser INATIVAR testes com zram
+NO_ZRAM_DEBUGGER=0 # Ative se quiser INATIVAR testes com zram
