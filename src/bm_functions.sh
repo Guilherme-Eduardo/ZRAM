9df7d9c84596fcab71bf372e9ae3ca6e971850eb
Gustavo Benitez Frehse <tuildes@localhost.localdomain>
2024-10-25 11:09:51 -0300

    fix: arrumado zram e suas impressoes em OS

--- src/bm_functions.sh
+++ src/bm_functions.sh
@@ -16,15 +16,52 @@ function write_logs_title() {
 function show_zram_stats() {
     AN=$1 # Nome do arquivo
 
+    if [[ $(zramctl) ]]; then
+
+	LOC="/sys/block/zram0"	
     # Impressao dos resultados
-    echo "---------------- RESULTADOS ZRAM (bytes) ---------------- " >> $AN
-    printf "%30s | %20d\n" "Swaps realizados" "$(cat /sys/block/zram0/stat)" >> $AN
-    printf "%30s | %20d\n" "Dados originais" "$(cat /sys/block/zram0/orig_data_size)" >> $AN
-    printf "%30s | %20d\n" "Dados comprimidos" "$(cat /sys/block/zram0/compr_data_size)" >> $AN
-    printf "%30s | %20d\n" "Memoria usada total" "$(cat /sys/block/zram0/mem_used_total)" >> $AN
-    printf "%30s | %20d\n" "Tamanho original" "$(cat /sys/block/zram0/orig_data_size)" >> $AN
-    printf "%30s | %20d\n" "Tamanho compactado" "$(cat /sys/block/zram0/compr_data_size)" >> $AN
-    echo "--------------------------------------------------------- " >> $AN
+    # echo "---------------- RESULTADOS ZRAM (bytes) ---------------- " >> $AN
+    # printf "%30s | %20d\n" "Swaps realizados" "$(cat /sys/block/zram0/stat)" >> $AN
+    # printf "%30s | %20d\n" "Dados originais" "$(cat /sys/block/zram0/orig_data_size)" >> $AN
+    # printf "%30s | %20d\n" "Dados comprimidos" "$(cat /sys/block/zram0/compr_data_size)" >> $AN
+    # printf "%30s | %20d\n" "Memoria usada total" "$(cat /sys/block/zram0/mem_used_total)" >> $AN
+    # printf "%30s | %20d\n" "Tamanho original" "$(cat /sys/block/zram0/orig_data_size)" >> $AN
+    # printf "%30s | %20d\n" "Tamanho compactado" "$(cat /sys/block/zram0/compr_data_size)" >> $AN
+    # echo "--------------------------------------------------------- " >> $AN
+
+	echo "---------------- RESULTADOS ZRAM (bytes) ---------------- " >> $AN
+
+	echo "More information: https://docs.kernel.org/admin-guide/blockdev/zram.html" >> $AN
+	echo "" >> $AN
+	
+	echo "DISKSIZE: " >> $AN
+	cat "${LOC}/disksize" >> $AN
+	echo "INITSTATE: " >> $AN
+    cat "${LOC}/disksize" >> $AN
+    echo "WRITEBACK_LIMIT_ENABLE: " >> $AN
+    cat "${LOC}/disksize" >> $AN
+    echo "MAX_COMP_STREAMS: " >> $AN
+    cat "${LOC}/disksize" >> $AN
+    echo "COMP_ALGORITHM: " >> $AN
+    cat "${LOC}/disksize" >> $AN
+    echo "DEBUG_STAT: " >> $AN
+    cat "${LOC}/disksize" >> $AN
+    echo "BACKING_DEV: " >> $AN
+    cat "${LOC}/disksize" >> $AN
+
+	# Precisa melhorar essas saidas
+    echo "STATS: " >> $AN
+    cat "${LOC}/stat" >> $AN
+	echo "STATS I/O: " >> $AN
+    cat "${LOC}/io_stat" >> $AN
+	echo "STATS mm: " >> $AN
+    cat "${LOC}/mm_stat" >> $AN
+	echo "STATS bd: " >> $AN
+    cat "${LOC}/bd_stat" >> $AN
+
+	echo "--------------------------------------------------------- " >> $AN
+
+	fi
 }
 
 ### MUDAR PORCENTAGEM DE ZRAM ###
@@ -43,15 +80,15 @@ function change_zram_porc() {
     fi
 
     # Verifica se eh OPENSUSE
-    if [ $(lsb_release -si 2>/dev/null) == "Open Suse" ]; then
-        if [ $PORC -eq 0 ]; then
+    if [ $(lsb_release -si 2>/dev/null) = 'openSUSE' ]; then
+	if [ $PORC -eq 0 ]; then
             sudo zramswapoff
+	    #systemctl disable --now zramswap.service
 
         # Habilitar zram
         else
-            sudo zramswapon
-
-            sudo swapoff /dev/zram0
+	    sudo zramswapoff
+	    # systemctl enable --now zramswap.service
 
             # Se precisar, habilita
             if ! lsmod | grep -q zram; then
@@ -61,34 +98,33 @@ function change_zram_porc() {
             # TODO: TROCAR PARA O ESPECIFICADO NO FUTURO
             echo 1 > /sys/block/zram0/reset
             echo 8G > /sys/block/zram0/disksize
-            sudo mkswap /dev/zram0
-            sudo swapon /dev/zram0
-        fi
-
-        return 0
-    fi
-
-    # Desabilitar zram
-    if [ $PORC -eq 0 ]; then
-        sudo systemctl disable --now zramswap.service 
-
-    # Habilitar zram
+            
+	    sudo zramswapon
+    	fi
     else
-        sudo systemctl enable --now zramswap.service
+    	# Desabilitar zram
+    	if [ $PORC -eq 0 ]; then
+        	sudo systemctl disable --now zramswap.service 
+    	# Habilitar zram
+    	else
+        	sudo systemctl enable --now zramswap.service
+        	sudo swapoff /dev/zram0
 
-        sudo swapoff /dev/zram0
+        	# Se precisar, habilita
+        	if ! lsmod | grep -q zram; then
+            	sudo modprobe zram
+        	fi
 
-        # Se precisar, habilita
-        if ! lsmod | grep -q zram; then
-            sudo modprobe zram
-        fi
-
-        # TODO: TROCAR PARA O ESPECIFICADO NO FUTURO
-        echo 1 > /sys/block/zram0/reset
-        echo 8G > /sys/block/zram0/disksize
-        sudo mkswap /dev/zram0
-        sudo swapon /dev/zram0
+        	# TODO: TROCAR PARA O ESPECIFICADO NO FUTURO
+       		echo 1 > /sys/block/zram0/reset
+        	echo 8G > /sys/block/zram0/disksize
+        	sudo mkswap /dev/zram0
+        	sudo swapon /dev/zram0
+    	fi
     fi
+	
+	write_logs $LOG_ARCHIVE "[INFO] zramctl:"
+	zramctl >> $LOG_ARCHIVE
 
     return 0
 }
