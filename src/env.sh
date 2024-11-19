32d94b8e00a62254051ba6cd01bc59bca0777e2c
Gustavo Frehse <frehsegustavo@gmail.com>
2024-11-19 13:44:43 -0300

    mod: lugar dos arquivos

--- src/env.sh
+++ src/env.sh
@@ -2,10 +2,11 @@
 
 ### Variaveis de ambiente ###
 
-BENCHMARKS=('bt' 'ch' 'dc' 'ep' 'ft' 'is' 'lu' 'mg' 'sp' 'ua') # Definir os benchmarks que serão executados
+# Removido DC
+# Algoritmo padrao de comprensaos: lzo-rle
+BENCHMARKS=('bt' 'cg' 'ep' 'ft' 'is' 'lu' 'mg' 'sp' 'ua') # Definir os benchmarks que serão executados
 BENCH_CLASSES=('C') # Classes a serem testadas (S (fast to test), A, B, C (medium 4x), D, E, F (large 16x))
-ZRAM_PORC=(0 50) # Porcentagem de ZRAM a serem testadas
-COMPRESSION_ALGS=('null')
+ZRAM_PORC=(0 25 50 75 100) # Porcentagem de ZRAM a serem testadas
 NUM_OF_REPETITIONS=100
 
 RESULT_SOURCE="../output" # Onde sera guardado os resultados (*.log)
@@ -20,5 +21,5 @@ CSV_RESULT="${RESULT_SOURCE}/$(date +"%d%m%Y_%H%M%S")_RESULT.csv"
 # Por enquanto desabilitar isto, apenas gera a nao habilitacao do zram
 # O teste continuara igual
 # Entao ele fara o teste para 50% de zram, mas se habilitar de fato
-NO_ZRAM_DEBUGGER=1 # 1 PARA DESABILITAR ZRAM
+NO_ZRAM_DEBUGGER=0 # 1 PARA DESABILITAR ZRAM
 NO_BENCHMARKS_RESULTS=1 # 1 PARA SEM DIRETORIO DE RESULTADOS PARA CADA BENCHMARK
